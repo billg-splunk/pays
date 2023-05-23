3e96e523d8ef05405e7d797eb273fe225adea6ef
Bill Grant <billg@splunk.com>
2023-05-23 08:33:34 -0400

    initial

---
+++ tracing.js
@@ -0,0 +1,29 @@
+const { NodeTracerProvider } = require('@opentelemetry/node');
+const { B3MultiPropagator } = require('@opentelemetry/propagator-b3');
+const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
+const { GrpcInstrumentation } = require('@opentelemetry/instrumentation-grpc');
+const { BatchSpanProcessor, ConsoleSpanExporter } = require('@opentelemetry/tracing');
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+
+const provider = new NodeTracerProvider();
+provider.register({
+  propagator: new B3MultiPropagator(),
+});
+
+registerInstrumentations({
+  instrumentations: [
+    new GrpcInstrumentation(),
+  ],
+});
+
+const exporter = new ZipkinExporter({
+  serviceName: 'paymentservice',
+  url: process.env.SIGNALFX_ENDPOINT_URL,
+});
+
+provider.addSpanProcessor(new BatchSpanProcessor(exporter));
+
+const CONSOLE_SPAN = process.env['CONSOLE_SPAN'];
+if (CONSOLE_SPAN === 'true') {
+  provider.addSpanProcessor(new BatchSpanProcessor(new ConsoleSpanExporter(), { bufferTimeout: 1000 }));
+}
