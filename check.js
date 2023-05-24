e3839dd3b8ce1c45dd237cc3fb99f88a79889c45
Bill Grant <billg@splunk.com>
2023-05-24 13:45:57 -0400

    revert

--- check.js
+++ check.js
@@ -32,9 +32,7 @@ module.exports = async function check(request) {
   try {
     //const location = ‘USA’;
     //const response = await axios.get(`${creditCheckServiceUrl}/test?location=${location}`);
-    //const response = await axios.get(`${creditCheckServiceUrl}/test`);
-    import fetch from 'node-fetch';
-    const response = await fetch(`${creditCheckServiceUrl}/test`);
+    const response = await axios.get(`${creditCheckServiceUrl}/test`);
     logger.info(
       { 'type': 'INFO',
         'operation': 'Derek-credit-check'
