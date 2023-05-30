ab1b6888fd7db14a7ef8d50c2129ff3ab329240a
Bill Grant <billg@splunk.com>
2023-05-30 14:15:03 -0400

    customer num and feature flag changes

--- check.js
+++ check.js
@@ -32,12 +32,39 @@ module.exports = async function check(request) {
   try {
     //const location = ‘USA’;
     //const response = await axios.get(`${creditCheckServiceUrl}/test?location=${location}`);
+    
+    // Changes for QEP
+    // (1) Randon number to decide if it's:
+    // - 0 to 5 --> Normal transaction
+    // - 6, 7 or 8 --> featureFlag = newTaxCode, newSpeedAlgo, and advancedMode
+    // - 9 --> hipsterCard workflows
+    iNum = Math.floor(Math.random() * 9) + 1;
+    // (2) Random number for rest of the customer number 6 digits
+    iCust = Math.floor((Math.random() * 900000) + 100000);
+    // (3) Concatenate and stringify
+    customerNum = "" + iCust + iNum;
+    // (4) Add feature flag
+    switch(iNum) {
+      case 6:
+        featureFlag = 'newTaxCode'
+	break;
+      case 7:
+        featureFlag = 'newSpeedAlgo'
+	break;
+      case 8:
+        featureFlag = 'advancedMode'
+	break;
+      default:
+        featureFlag = 'none'
+    }
+    // (5) Add attribute to span
     const grpcActiveSpan = getSpan(context.active());
     grpcActiveSpan.setAttributes({
-      'featureFlag': 'none'
+      'featureFlag': featureFlag
     });
-
-    const response = await axios.get(`${creditCheckServiceUrl}/check?customernum=1234567`);
+    // (6) Call creditcheckservice with customer num
+    const response = await axios.get(`${creditCheckServiceUrl}/check?customernum=${customerNum}`);
+    // (7) Log it out
     logger.info(
       { 'type': 'INFO',
         'operation': 'credit-check-service call'
